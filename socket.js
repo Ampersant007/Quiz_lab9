6d192cfe5e82f41cdcb386f2f2cbfa5959e28603
Egor Petrov <nepas106217@gmail.com>
2026-05-01 13:48:37 +0300

    add: russian.json

--- socket.js
+++ socket.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const TEST_FILE = 'tests/test1.json';
+const TEST_FILE = 'tests/russian.json';
 
 function loadTest() {
   try {
